b0c10e48b56b3d3592c5821ca373e78a06901b84
Laurin Wittich <laurin.leon.w@gmail.com>
2024-01-11 12:22:20 +0100

    Basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,31 +7,84 @@ public class PlayerController : MonoBehaviour
     public InputController inputController;
     private float timer;
     public float movementBlockTime = 0.5f;
+    public float force = 1f;
+
+    private bool isMoving = false;
+    private Vector3 right = new Vector3(2, 0, 0);
+    private Vector3 left = new Vector3(-2, 0, 0);
+    private Vector3 up = new Vector3(0, 1.5f, 0);
+
     void Jump()
     {
-        float currentTime = Time.deltaTime;
-        while (currentTime + movementBlockTime > Time.deltaTime)
-        {
-            gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0.1f, 0);
-        }
+        gameObject.GetComponent<Rigidbody>().AddForce(up * force, ForceMode.Impulse);
+        isMoving = true;
+        StartCoroutine(CooldownRoutine());
     }
 
     void Left()
     {
-
+        gameObject.GetComponent<Rigidbody>().AddForce(left * force, ForceMode.Impulse);
+        StartCoroutine(MoveBack());
+        isMoving = true;
+        StartCoroutine(CooldownRoutine());
     }
 
     void Right()
     {
-
+        gameObject.GetComponent<Rigidbody>().AddForce(right * force, ForceMode.Impulse);
+        StartCoroutine(MoveBack());
+        isMoving = true;
+        StartCoroutine(CooldownRoutine());
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(128f, 70.97f, -5f);
+    }
+
     void Update()
     {
-        if (inputController.inputDirectionLeftSide == InputController.Direction.Up && inputController.inputDirectionRightSide == InputController.Direction.Up)
+        if (!isMoving)
         {
-            Jump();
+            if (inputController.inputDirectionLeftSide == InputController.Direction.Up
+            && inputController.inputDirectionRightSide == InputController.Direction.Up)
+            {
+                Jump();
+            }
+            else if (inputController.inputDirectionLeftSide == InputController.Direction.Left
+            && inputController.inputDirectionRightSide == InputController.Direction.Down)
+            {
+                Left();
+            }
+            else if (inputController.inputDirectionLeftSide == InputController.Direction.Down
+            && inputController.inputDirectionRightSide == InputController.Direction.Right)
+            {
+                Right();
+            }
+        }
+    }
+
+    private IEnumerator CooldownRoutine()
+    {
+        while (isMoving)
+        {
+            yield return new WaitForSeconds(movementBlockTime);
+
+            isMoving = false;
+        }
+    }
+
+    private IEnumerator MoveBack()
+    {
+        yield return new WaitForSeconds(movementBlockTime / 2);
+
+        if (gameObject.transform.position.x < 128)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(right * force, ForceMode.Impulse);
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(left * force, ForceMode.Impulse);
         }
     }
 }
